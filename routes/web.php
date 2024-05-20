5fd2bc38d90a04a42ef94700e878c2ffb9e1c223
Nik0-9 <filannino.nicola9@gmail.com>
2024-05-20 18:21:02 +0200

    ADDED header.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nav_links = config('nav');
+    return view('home', compact('nav_links'));
 });
